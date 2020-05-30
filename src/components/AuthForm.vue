f5ee23eb30a752b9e089a5231c7131c05abba8cc
laura241 <laura241@protonmail.com>
2020-05-30 08:21:22 +0200

    adding nav bar with account and forum

---
+++ src/components/AuthForm.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Réseau social d'entreprise Groupomania</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AuthForm",
+};
+</script>
+
+<style></style>
---
+++ src/views/UserAccount.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="account">
+    <h1>Mon compte utilisateur</h1>
+  </div>
+</template>
+
+<script></script>
